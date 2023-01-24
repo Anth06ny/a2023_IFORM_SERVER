5a05b04441a0d3a5de8c2a80884b3da0cd33ae1b
Anthony <anth06ny@gmail.com>
2023-01-24 14:14:30 +0100

    Correction TP GET

--- src/main/java/com/example/a2023_iform_server/rest/MyRestController.java
+++ src/main/java/com/example/a2023_iform_server/rest/MyRestController.java
@@ -33,11 +33,44 @@ public class MyRestController {
 
     //http://localhost:8080/max?p1=5&p2=6
     @GetMapping("/max")
-    public int max(int p1, int p2) {
+    public int max(Integer p1, Integer p2) {
         System.out.println("/max p1=#" + p1 + "# p2=" + p2);
+
+        if(p1 == null){
+             return p2;
+        }
+        else if(p2 == null){
+             return p1;
+        }
 
         return Math.max(p1,p2);
     }
+
+//    //http://localhost:8080/max?p1=5&p2=6
+//    @GetMapping("/max")
+//    public int max(String p1, String p2) {
+//        System.out.println("/max p1=#" + p1 + "# p2=" + p2);
+//
+//        Integer p1Int = null;
+//        Integer p2Int = null;
+//        try {
+//            p1Int = Integer.parseInt(p1);
+//        } catch (Exception e) {
+//        }
+//
+//        try {
+//            p2Int = Integer.parseInt(p2);
+//        } catch (Exception e) {
+//        }
+//
+//        if (p1Int == null) {
+//            return p2Int;
+//        } else if (p2Int == null) {
+//            return p1Int;
+//        }
+//
+//        return Math.max(p1Int, p2Int);
+//    }
 
 
 }
